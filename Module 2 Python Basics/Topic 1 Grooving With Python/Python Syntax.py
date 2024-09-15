5510f9f833addd6572d0c7ac5594acaaf94c465a
anakeeSakura <sakura.sirisavath@gmail.com>
2024-09-15 00:40:15 -0500

    Posting Homework

---
+++ Module 2 Python Basics/Topic 1 Grooving With Python/Python Syntax.py
@@ -0,0 +1,40 @@
+# Task 1: Code Correction 
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+
+ #Task 2: Your Mood Today *
+Mood = input("how do you feel ")  
+if Mood == "happy":
+    print("That's great to hear!")
+elif Mood == "sad":
+    print("I hope your day gets better!")
+
+# 2. Python Naming Convention Practice
+# Task 1: Code Correction
+
+pi_value = 3.14
+user_age = 25
+user_location = "New York"
+max_limit = 1000
+
+# 3. Arithmetic Operations in Daily Life
+# Task 1: Grocery Store Math
+bread = 1
+peanut_butter = 2
+jelly = 3
+
+total_cost = bread + peanut_butter + jelly
+print(total_cost) 
+
+
+# Task 2: Bank Interest
+
+year_savings= 10000 * .07
+year_savings += 10000
+print(year_savings)
+
